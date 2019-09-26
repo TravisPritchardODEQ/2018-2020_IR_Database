d90b33a3cfdcf0c0d6a10dbe2a9ffdb240e0ebb6
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2019-09-26 15:45:44 -0700

    Update the sumary generation script
    Update to replaces monlocIDs that have been truncated with sci-notation. Pulls actual monloc names from database.

--- Tools/Assessment_summary.R
+++ Tools/Assessment_summary.R
@@ -9,7 +9,8 @@ library(openxlsx)
 # monitoring locations are taken from the ALLDATA data files in the 2018_WQAssessment/Draft List
 # folder
 
-options(scipen = 99999)
+options(scipen = 9999999)
+con <- DBI::dbConnect(odbc::odbc(), "IR 2018")
 
 # Load assessment result data
 all_bains_categories <- read.xlsx("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Draft List/Rollup/Basin_categories/ALL BASINS_categories.xlsx")
@@ -130,28 +131,28 @@ temp_spawn <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Draf
 Tox_AL_Ammonia <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Draft List/Tox_AL/Data_Review/TOX_AL_Ammonia_IR_Data_ALLDATA.csv",
                            stringsAsFactors = FALSE) %>%
   arrange(AU_ID, MLocID, SampleStartDate) %>%
-  select(AU_ID, MLocID, Char_Name, Pollu_ID, wqstd_code) %>%
+  select(AU_ID, MLocID, Char_Name, Pollu_ID, wqstd_code,Result_UID) %>%
   mutate(Pollu_ID = as.character(Pollu_ID),
          wqstd_code = as.character(wqstd_code))
 
 Tox_AL_CU <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Draft List/Tox_AL/Data_Review/TOX_AL_Cu_IR_Data_ALLDATA.csv",
                       stringsAsFactors = FALSE) %>%
   arrange(AU_ID, MLocID, SampleStartDate) %>%
-  select(AU_ID, MLocID, Char_Name, Pollu_ID, wqstd_code) %>%
+  select(AU_ID, MLocID, Char_Name, Pollu_ID, wqstd_code,Result_UID) %>%
   mutate(Pollu_ID = as.character(Pollu_ID),
          wqstd_code = as.character(wqstd_code))
 
 Tox_AL_Hardness_Metals <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Draft List/Tox_AL/Data_Review/TOX_AL_Hardness_Metals_IR_Data_ALLDATA.csv",
                                    stringsAsFactors = FALSE) %>%
   arrange(AU_ID, MLocID, SampleStartDate) %>%
-  select(AU_ID, MLocID, Char_Name, Pollu_ID, wqstd_code) %>%
+  select(AU_ID, MLocID, Char_Name, Pollu_ID, wqstd_code, Result_UID) %>%
   mutate(Pollu_ID = as.character(Pollu_ID),
          wqstd_code = as.character(wqstd_code))
 
 Tox_AL_Others <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Draft List/Tox_AL/Data_Review/TOX_AL_Others_IR_Data_ALLDATA.csv",
                           stringsAsFactors = FALSE) %>%
   arrange(AU_ID, MLocID, SampleStartDate) %>%
-  select(AU_ID, MLocID, Char_Name, Pollu_ID, wqstd_code) %>%
+  select(AU_ID, MLocID, Char_Name, Pollu_ID, wqstd_code,Result_UID) %>%
   mutate(Pollu_ID = as.character(Pollu_ID),
          wqstd_code = as.character(wqstd_code)) %>%
   mutate(Pollu_ID = case_when(Char_Name == 'Endosulfan'~ '77', 
@@ -161,16 +162,56 @@ Tox_AL_Others <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/D
 Tox_AL_Penta <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Draft List/Tox_AL/Data_Review/TOX_AL_Pentachlorophenol_IR_data_ALLDATA.csv",
                          stringsAsFactors = FALSE) %>%
   arrange(AU_ID, MLocID, SampleStartDate) %>%
-  select(AU_ID, MLocID, Char_Name, Pollu_ID, wqstd_code) %>%
+  select(AU_ID, MLocID, Char_Name, Pollu_ID, wqstd_code,Result_UID) %>%
   mutate(Pollu_ID = as.character(Pollu_ID),
          wqstd_code = as.character(wqstd_code))
 
-Tox_HH <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Draft List/Tox_HH/Data_Review/Tox_HH_IR_data_ALLDATA.csv",
+
+tox_AL_initial <- Tox_AL_Ammonia %>%
+  bind_rows(Tox_AL_CU) %>%
+  bind_rows(Tox_AL_Hardness_Metals) %>%
+  bind_rows(Tox_AL_Others) %>%
+  bind_rows(Tox_AL_Penta)
+
+
+
+
+MonLocs <-  DBI::dbGetQuery(con, "SELECT [Result_UID],
+                               [MLocID] as mon_station
+                               FROM [IntegratedReport].[dbo].[InputRaw]") 
+
+MonLocs <- distinct(MonLocs)
+
+
+tox_AL <- tox_AL_initial %>%
+  left_join(MonLocs) %>%
+  mutate(MLocID = mon_station) %>%
+  select(-mon_station, -Result_UID)
+
+  
+
+Tox_HH_initial <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Draft List/Tox_HH/Data_Review/Tox_HH_IR_data_ALLDATA.csv",
                    stringsAsFactors = FALSE) %>%
   arrange(AU_ID, MLocID, SampleStartDate) %>%
-  select(AU_ID, MLocID, Char_Name, Pollu_ID, wqstd_code) %>%
+  select(AU_ID, MLocID, Char_Name, Pollu_ID, wqstd_code,Result_UID ) %>%
   mutate(Pollu_ID = as.character(Pollu_ID),
          wqstd_code = as.character(wqstd_code))
+
+
+
+
+
+
+
+# HH_MonLocs <-  DBI::dbGetQuery(con, "SELECT [Result_UID],
+# 	   [MLocID] as mon_station
+#       
+#   FROM [IntegratedReport].[dbo].[VW_ToxHH]")
+
+Tox_HH <- Tox_HH_initial %>%
+  left_join(MonLocs) %>%
+  mutate(MLocID = mon_station) %>%
+  select(-mon_station, -Result_UID)
 
 Tox_HH_Hg_tissue <- read.csv("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Draft List/Tox_HH/Data_Review/Tox_HH_hg_tissue_IR_data_ALLDATA.csv",
                              stringsAsFactors = FALSE) %>%
@@ -195,7 +236,7 @@ biocriteria <- read.xlsx("//deqhq1/WQASSESSMENT/2018IRFiles/2018_WQAssessment/Dr
 
 #query from the database locations that have continuous data
 
-con <- DBI::dbConnect(odbc::odbc(), "IR 2018")
+
 
 all_temperature_data <- "SELECT [AU_ID], 
 OWRD_Basin, 
@@ -247,11 +288,7 @@ data_together <- bacteria_coast_contact %>%
   bind_rows(pH) %>%
   bind_rows(temp_year_round) %>%
   bind_rows(temp_spawn) %>%
-  bind_rows(Tox_AL_Ammonia) %>%
-  bind_rows(Tox_AL_CU) %>% 
-  bind_rows(Tox_AL_Hardness_Metals) %>% 
-  bind_rows(Tox_AL_Others) %>% 
-  bind_rows(Tox_AL_Penta) %>% 
+  bind_rows(tox_AL) %>%
   bind_rows(Tox_HH) %>% 
   bind_rows(Tox_HH_Hg_tissue) %>%
   bind_rows(AUs_with_no_7DADM) %>%
@@ -273,7 +310,7 @@ joined_BU_summary <- all_bains_categories %>%
                                 WQstd_code == "15" ~ paste0(Char_Name, "- Aquatic Life Criteria"),
                                 WQstd_code == "16" ~ paste0(Char_Name, "- Human Health Criteria"),
                                 TRUE ~ Char_Name)) %>%
-  select(AU_ID, AU_Name, OWRD_Basin, Char_Name,  Assessment,IR_category, Monitoring_locations,Year_listed, Assessed_in_2018 )
+  select(AU_ID, AU_Name, AU_Description, OWRD_Basin, Char_Name,  Assessment,IR_category, Monitoring_locations,Year_listed, Assessed_in_2018 )
 
 
 save(joined_BU_summary, file = "data/assessment_display.Rdata")
