b193b1972b660d991812bc7ce1ecedbc9a9f99e9
PRITCHARD Travis <TPRITCH@deq.state.or.us>
2020-01-06 08:44:33 -0800

    undoing custom colors
    They looked bad

--- app.R
+++ app.R
@@ -230,36 +230,36 @@ tabPanel("Raw Data Download",
 
 
 ,add_busy_spinner(spin = "fading-circle")
-,
-
-
-
-# bootstrapPage('',
+# ,
 # 
-#               tags$style(type = 'text/css', ".navbar { background-color: #71bcb4;}",
-#                        ".navbar-default .navbar-nav > .active > a",
-#                        ".navbar-default .navbar-nav > .active > a:focus",
-#                        ".navbar-default .navbar-nav > .active > a:hover {color: pink;background-color: purple;}",
-#                        ".navbar-default .navbar-nav > li > a:hover {color: black;background-color:yellow;text-decoration:underline;}",
-#                        ".navbar-default .navbar-nav > li > a[data-value='t1'] {color: red;background-color: pink;}",
-#                        ".navbar-default .navbar-nav > li > a[data-value='t2'] {color: blue;background-color: lightblue;}",
-#                        ".navbar-default .navbar-nav > li > a[data-value='t3'] {color: green;background-color: lightgreen;"
-#                          
-#               )
-
-tags$style(type = 'text/css', 
-           HTML('.navbar { background-color: #71bcb4;}
-                          .navbar-default .navbar-brand{color: black;}
-                          .tab-panel{ background-color: red; color: black}
-                          .navbar-default .navbar-nav > .active > a, 
-                           .navbar-default .navbar-nav > .active > a:focus, 
-                           .navbar-default .navbar-nav > .active > a:hover {
-                                color: black;
-                                background-color: #00907e;
-                            }')
-           
-           
-)
+# 
+# 
+# # bootstrapPage('',
+# # 
+# #               tags$style(type = 'text/css', ".navbar { background-color: #71bcb4;}",
+# #                        ".navbar-default .navbar-nav > .active > a",
+# #                        ".navbar-default .navbar-nav > .active > a:focus",
+# #                        ".navbar-default .navbar-nav > .active > a:hover {color: pink;background-color: purple;}",
+# #                        ".navbar-default .navbar-nav > li > a:hover {color: black;background-color:yellow;text-decoration:underline;}",
+# #                        ".navbar-default .navbar-nav > li > a[data-value='t1'] {color: red;background-color: pink;}",
+# #                        ".navbar-default .navbar-nav > li > a[data-value='t2'] {color: blue;background-color: lightblue;}",
+# #                        ".navbar-default .navbar-nav > li > a[data-value='t3'] {color: green;background-color: lightgreen;"
+# #                          
+# #               )
+# 
+# tags$style(type = 'text/css', 
+#            HTML('.navbar { background-color: #71bcb4;}
+#                           .navbar-default .navbar-brand{color: black;}
+#                           .tab-panel{ background-color: red; color: black}
+#                           .navbar-default .navbar-nav > .active > a, 
+#                            .navbar-default .navbar-nav > .active > a:focus, 
+#                            .navbar-default .navbar-nav > .active > a:hover {
+#                                 color: black;
+#                                 background-color: #00907e;
+#                             }')
+#            
+#            
+# )
             
 )
 
